71a0a17ea3c414fea19b212349f38438c171f960
Nathan Sass <nsass711@gmail.com>
2016-02-28 18:03:08 +0900

    Refactor: moved the routes to their own files

--- models/event.js
+++ models/event.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var EventSchema   = new Schema({
-    name: String
+    title: String
 });
 
 module.exports = mongoose.model('Event', EventSchema);
